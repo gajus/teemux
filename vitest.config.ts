d8a9fc23931f96ef141ae83e154446d5af4e825c
Gajus <gajus@gajus.com>
2026-01-09 11:38:32 -0600

    fix: remove invalid option

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     exclude: ['**/e2e/**', '**/node_modules/**'],
-    singleThread: true,
     testTimeout: 5_000,
   },
 });
