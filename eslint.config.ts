2ca8c0f8abd09a5c3ef4177d3147887f31182ef5
Gajus <gajus@gajus.com>
2026-01-23 07:31:31 -0600

    chore: correct lint scripts

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,8 +6,11 @@ export default defineConfig([
   {
     ignores: [
       'dist',
+      'package-lock.json',
       'pnpm-lock.yaml',
+      'scripts',
       'src/client/styled-system',
+      'src/client/vite.config.ts',
       'test-results',
     ],
   },
